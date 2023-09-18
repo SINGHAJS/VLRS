235bbbff2d53ab07648a13f2b8e2e25417237819
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-09-18 19:18:22 +1200

    adds a map screen with osm map

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 18,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        )
+      ],
+    );
+  }
+}
