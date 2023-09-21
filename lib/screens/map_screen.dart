ee38ffe13f91c3b577496867467dd50dabb257a0
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-09-22 11:11:24 +1200

    uses the mqtt_client_service class to establish connection to the broker

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:vlrs/services/geolocation_service.dart';
 import 'package:lottie/lottie.dart' as lottie;
+import 'package:vlrs/services/mqtt_client_service.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -13,13 +14,32 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final GeolocationService _geolocationService = GeolocationService();
-  bool _isLocationDataLoading = true;
   late LatLng _userLatLng;
+
+  bool _isLocationDataLoading = true;
+  bool _isMqttClientConnected = false;
+
+  final String _brokerURL = '43.226.218.94';
+  final String _clientId = 'bfa1ffb0-585a-11ee-8816-b3b2ecd2ae97';
+  final int _port = 1883;
+  final String _accessToken = 'TRB8zmWVjcJYFssMtGCX';
+  late MQTTClientService _mqttClientService;
+  final String _pubTopic = 'v1/devices/me/telemetry';
 
   @override
   void initState() {
     super.initState();
     _getUserLocation(); // Call the method to fetch the user's location
+    _mqttClientService =
+        MQTTClientService(_brokerURL, _clientId, _accessToken, _port);
+    _mqttClientService.establishConnection();
+    // _establishClientConnection();
+  }
+
+  @override
+  void dispose() {
+    _mqttClientService.terminateClientConnection();
+    super.dispose();
   }
 
   // Method to fetch and set the user's location
@@ -29,6 +49,17 @@ class _MapScreenState extends State<MapScreen> {
       _userLatLng = userLocation; // Set user's location
       _isLocationDataLoading = false; // Set the data loading to false
     });
+  }
+
+  Future<void> _establishClientConnection() async {
+    await _mqttClientService.establishConnection();
+    setState(() {
+      _isMqttClientConnected = true;
+    });
+
+    if (_isMqttClientConnected) {
+      // _mqttClientService.publishMessage(_pubTopic, "{flutter: ${303.00}}");
+    }
   }
 
   @override
