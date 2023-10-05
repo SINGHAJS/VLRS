166578c13c794e4af9d3da1693191a54eee7503e
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-05 17:23:28 +1300

    adds mqtt constants

---
+++ lib/constants/mqtt_constants.dart
@@ -0,0 +1,6 @@
+class MqttConstants {
+  static const String HOSTNAME = '43.226.218.94';
+  static const String CLIENT_ID = 'bfa1ffb0-585a-11ee-8816-b3b2ecd2ae97';
+  static const String ACCESS_TOKEN = 'TRB8zmWVjcJYFssMtGCX';
+  static const int PORT = 1883;
+}
