f44bf434a2184cd833f0f52bbd1e23525445d46f
Jason Christian <jasonchristian1234@gmail.com>
2023-10-09 20:55:47 +1300

    fix: automatically renew JWT token

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,25 +2,14 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-
-// Geolocation and map
 import 'package:flutter_map/flutter_map.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:logger/logger.dart';
+import 'package:lottie/lottie.dart' as lottie;
+import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:vlrs/services/geolocation_service.dart';
 
-// Animation
-import 'package:lottie/lottie.dart' as lottie;
-
-// Web Socket
-import 'package:web_socket_channel/web_socket_channel.dart';
-
 import '../config/constants.dart';
-
-// Mqtt
-import 'package:vlrs/services/mqtt_service.dart';
-import 'package:vlrs/constants/mqtt_constants.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -30,26 +19,29 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  // Logger
-  var _logger = Logger();
-
-  // Geolocation
+  // instance of geolocation service
   final GeolocationService _geolocationService = GeolocationService();
+
+  // variable to store user location
   late LatLng _userLatLng;
 
-  // Mqtt
-  late MqttService _mqttClientService;
-  late MapController _mapController;
+  // map controller to move map center
+  late MapController? _mapController;
+
+  // instance of hive box to store jwt token and expired token
   final box = Hive.box('vlrs');
 
-  // Flags
-  bool _isLocationDataLoading = true;
-  bool _isMqttClientConnected = false;
+  // variable to store jwt token
+  String? jwtToken;
 
-  final _telemetryStream = WebSocketChannel.connect(Uri.parse(
-      "ws://43.226.218.94:8080/api/ws/plugins/telemetry?token=$token"));
+  // variable stream to listen datetime now
+  late Stream<DateTime> stream;
 
-  void _gettelemetryStream() {
+  // get telemetry data from thingsboard server
+  WebSocketChannel _telemetryStream() {
+    var uri = Uri.parse(
+        "ws://43.226.218.94:8080/api/ws/plugins/telemetry?token=$jwtToken");
+    final telemetryStream = WebSocketChannel.connect(uri);
     var object = {
       "tsSubCmds": [
         {
@@ -62,60 +54,135 @@ class _MapScreenState extends State<MapScreen> {
       "historyCmds": [],
       "attrSubCmds": []
     };
-    var data = jsonEncode(object);
-    _telemetryStream.sink.add(data);
+    var data = jsonEncode(object); // convert object to json
+    telemetryStream.sink.add(data); // send data to thingsboard server
+    return telemetryStream; // return telemetry stream to use in stream builder
   }
 
-  void _checkJwtToken() {
-    var jwtToken = box.get('jwtToken');
-    final dio = Dio();
+  // check if jwt token is null or expired
+  void checkToken() async {
+    // get jwt token from hive box
+    jwtToken = box.get('token');
+
+    // if jwt token is null, send request to get new jwt token
     if (jwtToken == null) {
-      var result   =dio.post("http://43.226.218.94:8080/api/auth/login");
+      // get new jwt token
+      String? token = await fetchJwtToken();
+      if (token != null) {
+        // if success, store jwt token and expired token to hive box
+        box.put('token', token);
+        jwtToken = token;
+        var expiredAt = DateTime.now().add(const Duration(minutes: 90));
+        var stringExpireAt = expiredAt.toIso8601String();
+        box.put('expiredToken', stringExpireAt);
+      }
+    } else {
+      // if jwt token is not null, check if jwt token is expired
+      var expiredAt = box.get('expiredToken');
+      var expiredAtDateTime = DateTime.parse(expiredAt);
+      var now = DateTime.now();
+
+      if (now.isAfter(expiredAtDateTime)) {
+        // if expired, send request to get new jwt token
+        resendRequest();
+      } else {
+        // if not expired, check every minute if jwt token is expired
+        stream = Stream<DateTime>.periodic(
+          const Duration(minutes: 1),
+          (count) => DateTime.now(),
+        );
+        stream.listen((event) {
+          if (event.isAfter(expiredAtDateTime)) {
+            resendRequest();
+
+            // close stream
+            stream.drain();
+          }
+        });
+      }
     }
+  }
+
+  // resend request to get new jwt token if jwt token is expired or null
+  resendRequest() async {
+    var jwtToken = await fetchJwtToken();
+    this.jwtToken = jwtToken;
+    box.put('token', jwtToken);
+    checkToken();
+  }
+
+  // send request to thingsboard server to get jwt token
+  Future<String?> fetchJwtToken() async {
+    final dio = Dio();
+    const url = 'http://43.226.218.94:8080/api/auth/login'; // thingsboard url
+
+    final data = {
+      // change this to your username and password of your thingsboard account
+      'username': 'tenant@thingsboard.org',
+      'password': 'tenant',
+    };
+
+    try {
+      // send post request to thingsboard server to get jwt token
+      // if success, return jwt token
+      // if failed, return null
+      final response = await dio.post(
+        url,
+        data: data,
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        return response.data['token'];
+      } else {
+        return null;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // get user location
+  Future<void> _getUserLocation() async {
+    LatLng currentLatLng = await _geolocationService.getCurrentUserLocation();
+    setState(() {
+      _userLatLng = currentLatLng;
+    });
+  }
+
+  // move map center based on lat and lng
+  void moveMapCenter(double lat, double lng) {
+    Future.delayed(const Duration(seconds: 3), () {
+      _mapController!.move(LatLng(lat, lng), 18);
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    _mapController = MapController(); // Initialise the map controller
-    _gettelemetryStream();
-    _getUserLocation(); // Call the method to fetch the user's location
-
-    _mqttClientService = MqttService(
-        MqttConstants.HOSTNAME,
-        MqttConstants.CLIENT_ID,
-        MqttConstants.ACCESS_TOKEN,
-        MqttConstants.PORT); // Initialise the mqtt client service
-
-    _setUpMqttCommunication(); // Call the setup method to set up the mqtt communication
+    _mapController = MapController();
+    _getUserLocation();
+    checkToken();
   }
 
-  // Method to fetch and set the user's location
-  Future<void> _getUserLocation() async {
-    _userLatLng = await _geolocationService.getCurrentUserLocation();
-    setState(() {
-      _isLocationDataLoading = false; // Set the data loading to false
-    });
-  }
-
-  Future<void> _setUpMqttCommunication() async {
-    _isMqttClientConnected = await _mqttClientService.establishConnection();
-
-    if (_isMqttClientConnected) {
-      setState(() {
-        // Perform publish or subscription
-      });
-    }
+  @override
+  void dispose() {
+    // dispose map controller and stream
+    _mapController!.dispose();
+    stream.drain();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
       body: StreamBuilder(
-        stream: _telemetryStream.stream,
+        stream: _telemetryStream().stream,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
@@ -123,8 +190,6 @@ class _MapScreenState extends State<MapScreen> {
             );
           }
           if (snapshot.hasData == false) {
-            _gettelemetryStream();
-            // Show a loading indicator while waiting for user and other devices location
             return Center(
               child: lottie.Lottie.asset(
                   "assets/animations/animation_lmpkib5u.json"),
@@ -135,26 +200,30 @@ class _MapScreenState extends State<MapScreen> {
               child: Text("Error"),
             );
           }
+
+          // get telemetry data from thingsboard server
           var json = jsonDecode(snapshot.data);
           var data = json['data'];
           var stringLat = data["latitude"][0][1];
           var stringLng = data["longitude"][0][1];
+
+          // convert string to double
           var lat = double.parse(stringLat);
           var lng = double.parse(stringLng);
+
+          // move map center based on lat and lng
           moveMapCenter(lat, lng);
-          _logger.d('Latitude $stringLat');
-          _logger.d('Longitude $stringLng');
+
+          // return flutter map widget to display map
           return FlutterMap(
             mapController: _mapController,
             options: MapOptions(
-              center:
-                  LatLng(lat, lng), // Use the current location as the center
-              zoom: 18, // Default zoom level
-              maxZoom: 18, // Maximum zoom level
-              minZoom: 14, // Minimum zoom level
+              center: LatLng(lat, lng),
+              zoom: 14,
+              maxZoom: 18,
+              minZoom: 14,
             ),
             children: [
-              // Sets the map layout
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.vlrs.app',
@@ -162,8 +231,7 @@ class _MapScreenState extends State<MapScreen> {
               MarkerLayer(
                 markers: [
                   Marker(
-                    point: LatLng(lat,
-                        lng), // Sets the marker on the user's current location
+                    point: LatLng(lat, lng),
                     width: 80,
                     height: 80,
                     builder: (context) => const Icon(
@@ -171,43 +239,6 @@ class _MapScreenState extends State<MapScreen> {
                       size: 35.0,
                       color: Colors.red,
                     ),
-                  ),
-                ],
-              ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point:
-                        _userLatLng, // Sets the marker on the user's current location
-                    width: 80,
-                    height: 80,
-                    builder: (context) => const Icon(
-                      Icons.my_location,
-                      size: 35.0,
-                      color: Colors.red,
-                    ),
-                  ),
-                ],
-              ),
-              CircleLayer(
-                circles: [
-                  CircleMarker(
-                    // Set the outer circler for the marker of the user's location
-                    point: _userLatLng,
-                    radius: 8,
-                    useRadiusInMeter: true,
-                    color: const Color.fromRGBO(255, 255, 255, 1),
-                  ),
-                ],
-              ),
-              CircleLayer(
-                circles: [
-                  CircleMarker(
-                    // Set the inner circle for the marker of the user's location
-                    point: _userLatLng,
-                    radius: 6,
-                    useRadiusInMeter: true,
-                    color: const Color.fromRGBO(33, 150, 243, 1),
                   ),
                 ],
               ),
@@ -216,11 +247,5 @@ class _MapScreenState extends State<MapScreen> {
         },
       ),
     );
-  }
-
-  void moveMapCenter(double lat, double lng) {
-    Future.delayed(const Duration(milliseconds: 1500), () {
-      _mapController.move(LatLng(lat, lng), 18);
-    });
   }
 }
