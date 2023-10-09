0fd481d4979ae8b0b5e159af4463a4b49305c36a
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-10 02:28:39 +1300

    adds constants file for jwt and websocket

---
+++ lib/constants/websocket_constants.dart
@@ -0,0 +1,7 @@
+class WebSocketConstants {
+  static const String _hostname = 'ws://43.226.218.94:8080/';
+  static const String _apiEndPoint = 'api/ws/plugins/telemetry?token=';
+  static const String WS_URI = '$_hostname$_apiEndPoint';
+  static const String PUBLISHER_DEVICE_ID =
+      '630a8b30-46be-11ee-8816-b3b2ecd2ae97';
+}
