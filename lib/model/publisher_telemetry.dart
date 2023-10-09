db4b79031d2da7a63b602c1e0c01b08d59ea756d
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-10 02:30:16 +1300

    adds a telemetry model

---
+++ lib/model/publisher_telemetry.dart
@@ -0,0 +1,15 @@
+class PublisherTelemetry {
+  final double bearing;
+  final String direction;
+  final double latitude;
+  final double longitude;
+  final double speed;
+
+  PublisherTelemetry({
+    required this.bearing,
+    required this.direction,
+    required this.latitude,
+    required this.longitude,
+    required this.speed,
+  });
+}
