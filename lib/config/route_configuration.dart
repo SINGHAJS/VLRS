f28f27f3c3e2092b53b2f0030a3f1a75f0ba1d61
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-10 13:08:25 +1300

    adds route path to the splash screen

--- lib/config/route_configuration.dart
+++ lib/config/route_configuration.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vlrs/screens/map_screen.dart';
+import 'package:vlrs/screens/splash_screen.dart';
 
 final GoRouter router = GoRouter(
   routes: <RouteBase>[
@@ -9,6 +10,12 @@ final GoRouter router = GoRouter(
         name: 'Map',
         builder: (BuildContext context, GoRouterState state) {
           return const MapScreen();
+        }),
+    GoRoute(
+        path: '/',
+        name: 'Splash Screen',
+        builder: (BuildContext context, GoRouterState state) {
+          return const SplashScreen();
         })
   ],
 );
