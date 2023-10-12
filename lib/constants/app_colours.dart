10ebe7f859bece1c04806b4784d9cf90227bafca
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-12 20:47:13 +1300

    adds app colours constants file

---
+++ lib/constants/app_colours.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const Color BACKGROUND_PRIMARY_COLOUR = Color(0xFF000814);
+  static const Color TEXT_PRIMARY_COLOR = Color(0xFFEDF2F4);
+  static const Color ACCENT_COLOUR = Color(0xFFD90429);
+}
