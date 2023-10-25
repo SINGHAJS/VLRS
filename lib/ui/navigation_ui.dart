a9f5ba7d57dbe7259c6bcc87ce22d432791742df
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-26 11:45:18 +1300

    refactors navigation ui

--- lib/ui/navigation_ui.dart
+++ lib/ui/navigation_ui.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:vlrs/controllers/navigation/navigation_controller.dart';
+import 'package:vlrs/controllers/navigation_controller.dart';
 import 'package:vlrs/constants/app_colours.dart';
 
 class NavigationUI {
   final NavigationController _navigationController = NavigationController();
 
+  ///
+  /// This function shows a navigation bar with the map, message, and settings icons.
+  /// This function ui is used to navigate between different screens in the application.
+  ///
+  /// Param: [context], used for onPressed handlers.
+  ///
+  /// Return: Widget, Align with Row.
+  ///
   Widget showNavigationBar(BuildContext context) {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
         height: 60,
-        width: 250,
+        width: 350,
         margin: const EdgeInsets.only(bottom: 16.0),
         decoration: BoxDecoration(
           color: AppColours.BACKGROUND_PRIMARY_COLOUR,
@@ -21,18 +29,18 @@ class NavigationUI {
           children: [
             IconButton(
               icon: const Icon(Icons.map),
-              color: AppColours.TEXT_PRIMARY_COLOR,
+              color: AppColours.TEXT_SECONDARY_COLOR,
               onPressed: () => _navigationController.onMapClickHandler(context),
             ),
             IconButton(
               icon: const Icon(Icons.chat),
-              color: AppColours.TEXT_PRIMARY_COLOR,
+              color: AppColours.TEXT_SECONDARY_COLOR,
               onPressed: () =>
                   _navigationController.onMessageClickHandler(context),
             ),
             IconButton(
               icon: const Icon(Icons.settings),
-              color: AppColours.TEXT_PRIMARY_COLOR,
+              color: AppColours.TEXT_SECONDARY_COLOR,
               onPressed: () =>
                   _navigationController.onSettingsClickHandler(context),
             ),
