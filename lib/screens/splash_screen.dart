c4abc9b57705f3d3587188311a7677e4b50202f2
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-10-26 11:43:46 +1300

    refactors splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:vlrs/constants/app_colours.dart';
+import 'package:vlrs/ui/splash_ui.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    void onContinueClickHandler() {
-      context.go('/map');
-    }
+    // UI
+    SplashUI _splashUI = SplashUI();
 
     return Scaffold(
       body: Center(
         child: Container(
-          color: AppColours.BACKGROUND_PRIMARY_COLOUR,
+          decoration: const BoxDecoration(
+            gradient: AppColours.BACKGROUND_PRIMARY_GRADIENT,
+          ),
           width: double.infinity, // Take up full width
           height: double.infinity, // Take up full height
           child: Column(
@@ -24,46 +25,9 @@ class SplashScreen extends StatelessWidget {
                 CrossAxisAlignment.center, // Center horizontally
             children: <Widget>[
               // Load application icon image from assets.
-              Image.asset(
-                'assets/images/splash_screen/earth_pin_location.png',
-                width: 200,
-                height: 200,
-              ),
-              const Column(
-                children: [
-                  Text(
-                    'WELCOME TO VLRS!',
-                    style: TextStyle(
-                      color: AppColours.ACCENT_COLOUR,
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'VEHICLE LOCATION REPORTING SYSTEM',
-                    style: TextStyle(
-                      color: AppColours.TEXT_PRIMARY_COLOR,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                width: 150,
-                height: 40,
-                child: ElevatedButton(
-                  onPressed: onContinueClickHandler,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColours.ACCENT_COLOUR,
-                  ),
-                  child: const Text(
-                    'CONTINUE',
-                    style: TextStyle(
-                      color: AppColours.TEXT_PRIMARY_COLOR,
-                    ),
-                  ),
-                ),
-              ),
+              _splashUI.showSplashScreenIcon(),
+              _splashUI.showSplashScreenHeadings(),
+              _splashUI.showContinueButton(context),
             ],
           ),
         ),
