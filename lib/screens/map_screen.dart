3140369d05bcff8fdef2dee86bc8d422288a3661
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-11-04 17:40:51 +1300

    updates map screen to show routes on the map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -6,9 +6,11 @@ import 'package:vlrs/services/geolocation_service.dart';
 import 'package:logger/logger.dart';
 import 'package:vlrs/services/websocket_service.dart';
 import 'package:vlrs/model/publisher_telemetry.dart';
+import 'package:vlrs/ui/map_route_ui.dart';
 import 'package:vlrs/ui/map_ui.dart';
 import 'package:vlrs/ui/loading_ui.dart';
 import 'package:vlrs/ui/navigation_ui.dart';
+import 'package:vlrs/utils/json_utils.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -23,10 +25,16 @@ class _MapScreenState extends State<MapScreen> {
 
   // Flags
   bool _isUserLocationAttained = false;
+  bool _isRouteDataReceived = false;
 
   // Geolocation
   final GeolocationService _geolocationService = GeolocationService();
   late LatLng _userLatLng;
+
+  // Map
+  final JsonUtils _jsonUtils = JsonUtils();
+  late List<LatLng> _mapRouteToData;
+  late List<LatLng> _mapRouteFromData;
 
   // WebSocket
   final WebSocketService _webSocketService = WebSocketService();
@@ -34,22 +42,44 @@ class _MapScreenState extends State<MapScreen> {
   // Models
   late PublisherTelemetry _publisherTelemetry;
 
+  // Paths
+  final String _routeCoordinatesToPath =
+      'assets/coordinates/RouteCoordinatesTo.json';
+  final String _routeCoordinatesFromPath =
+      'assets/coordinates/RouteCoordinatesFrom.json';
+
   // UI
   final MapUI _mapUI = MapUI();
   final LoadingUI _loadingUI = LoadingUI();
   final NavigationUI _navigationUI = NavigationUI();
+  final MapRouteUI _mapRouteUI = MapRouteUI();
 
   @override
   void initState() {
     super.initState();
     _getUserLocation();
     _webSocketService.validateToken();
+    _getRoutesData();
   }
 
   @override
   void dispose() {
     _webSocketService.disposeWebSocketConnection;
     super.dispose();
+  }
+
+  ///
+  /// Reads the coordinates file of the routes, initializes it, and updates the
+  /// [_isRouteDataReceived] flag.
+  ///
+  Future<void> _getRoutesData() async {
+    _mapRouteToData =
+        await _jsonUtils.readLatLngFromJson(_routeCoordinatesToPath);
+    _mapRouteFromData =
+        await _jsonUtils.readLatLngFromJson(_routeCoordinatesFromPath);
+    setState(() {
+      _isRouteDataReceived = true;
+    });
   }
 
   ///
@@ -82,6 +112,7 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // print(_mapRouteToData);
     return Scaffold(
       body: StreamBuilder(
         stream: _webSocketService.telemetryStream().stream,
@@ -93,8 +124,10 @@ class _MapScreenState extends State<MapScreen> {
             return Stack(children: <Widget>[
               FlutterMap(
                 options: MapOptions(
-                  center: LatLng(_publisherTelemetry.latitude,
-                      _publisherTelemetry.longitude),
+                  // center: LatLng(_publisherTelemetry.latitude,
+                  //     _publisherTelemetry.longitude),
+                  // center: _userLatLng,
+                  center: const LatLng(-36.785334, 175.023230),
                   zoom: 17, // Default Zoom
                   maxZoom: 17, // Max Zoom
                   minZoom: 14, // Min Zoom
@@ -115,9 +148,12 @@ class _MapScreenState extends State<MapScreen> {
                     ],
                   ),
                   _mapUI.showUserCircleLayerOnMapUI(_userLatLng),
+                  _mapRouteUI.drawVehicleRoute(_mapRouteToData, Colors.blue),
+                  _mapRouteUI.drawVehicleRoute(_mapRouteFromData, Colors.blue),
                 ],
               ),
-              _navigationUI.showNavigationBar(context),
+              // _navigationUI.showNavigationBar(context),
+              // _mapRouteUI.drawVehicleRoute(),
             ]);
           }
         },
