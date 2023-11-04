f1257f81f2991cb7d21ace3ed303e78372538b2c
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-11-04 17:40:12 +1300

    adds a function to read the json files

--- lib/utils/json_utils.dart
+++ lib/utils/json_utils.dart
@@ -4,9 +4,17 @@ import 'dart:convert';
 import 'dart:async' show Future;
 
 class JsonUtils {
-  Future<List<LatLng>> readLatLngFromJson() async {
-    final String jsonString =
-        await rootBundle.loadString('assets/coordinates/RouteCoords.json');
+
+  ///
+  /// This function takes a filename that contains the coordinates
+  /// data and adds the coordinates as Lat and Lng objects into a list. 
+  /// 
+  /// Param: [fileName], path to the file
+  /// 
+  /// Return: List<LatLng>, list of LatLng objects
+  /// 
+  Future<List<LatLng>> readLatLngFromJson(String fileName) async {
+    final String jsonString = await rootBundle.loadString(fileName);
 
     final Map<String, dynamic> jsonData = json.decode(jsonString);
     final List<LatLng> latLngs = [];
@@ -19,14 +27,6 @@ class JsonUtils {
         latLngs.add(latLng);
       }
     });
-
     return latLngs;
-  }
-
-  void extractTelemetryData(dynamic snapshotData) {
-    var json = jsonDecode(snapshotData);
-    var data = json['data'];
-    var stringLat = data["latitude"][0][1];
-    var stringLng = data["longitude"][0][1];
   }
 }
