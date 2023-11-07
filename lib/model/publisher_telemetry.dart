d3f99a1ae00046056287b441ec1b78ec87b52411
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2023-11-07 17:10:48 +1300

    adds a new name attribute to the model

--- lib/model/publisher_telemetry.dart
+++ lib/model/publisher_telemetry.dart
@@ -4,8 +4,10 @@ class PublisherTelemetry {
   final double latitude;
   final double longitude;
   final double speed;
+  final String busName;
 
   PublisherTelemetry({
+    this.busName = 'Bus X',
     required this.bearing,
     required this.direction,
     required this.latitude,
