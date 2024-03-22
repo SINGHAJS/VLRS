d93238a0e5f09d15a1e728a67f2df7e47c739e8e
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2024-03-22 18:11:01 +1300

    updates websocket constants

--- lib/constants/websocket_constants.dart
+++ lib/constants/websocket_constants.dart
@@ -3,7 +3,7 @@ class WebSocketConstants {
   static const String _apiEndPoint = 'api/ws/plugins/telemetry?token=';
   static const String WS_URI = '$_hostname$_apiEndPoint';
   static const String PUBLISHER_DEVICE_ID_A =
-      '630a8b30-46be-11ee-8816-b3b2ecd2ae97';
+      'dfe1cff0-71a3-11ee-8816-b3b2ecd2ae97';
   static const String PUBLISHER_DEVICE_ID_B =
       'd7c0f630-638d-11ee-8816-b3b2ecd2ae97';
 }
