cafbd49e197e9fc6d3172b2e1f559afaaaa85f45
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2024-03-22 18:59:44 +1300

    changes the title of the application

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: 'VLRS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Roboto',
