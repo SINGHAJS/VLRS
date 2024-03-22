5e0ae490935312759ebb51bdb6b8e6c2957a0d31
Ajit Singh <ajitsingh@Ajits-MacBook-Pro.local>
2024-03-22 18:10:05 +1300

    adds tom tom api constants

---
+++ lib/constants/tomtom_constants.dart
@@ -0,0 +1,5 @@
+class TomTomConstants {
+  static const String API_KEY = 'sjbSNrlLa0ONq0LT4bDRa8k3RyNYwIUc';
+  static const String BASE_ROUTE_API_URL =
+      'api.tomtom.com/routing/1/calculateRoute';
+}
